839f49ec2d2d7ef1afad301a6d26e4fd4fe0f63e
Sissy Dancheva <komandira123@gmail.com>
2018-11-12 00:14:30 +0200

    Add project files.

---
+++ 01_Task_Biggest_number_of_2_with_if/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Task_Biggest_number_of_2_with_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Changes the places of the two numbers if the first one is bigger.\n");
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                int c = a;
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine("First number - {0}, Second number - {1}.", a, b);
+        }
+    }
+}
---
+++ 02_Task_+_or_-_of_production_of_2_numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_Task___or___of_production_of_2_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a < 0 && b < 0 && c < 0) Console.WriteLine("-");
+            else if (a >= 0 && b >= 0 && c >= 0) Console.WriteLine("+");
+            else if (a < 0 && b < 0 && c >= 0) Console.WriteLine("+");
+            else if (a < 0 && b >= 0 && c < 0) Console.WriteLine("+");
+            else if (a >= 0 && b < 0 && c < 0) Console.WriteLine("+");
+            else if (a < 0 && b >= 0 && c >= 0) Console.WriteLine("-");
+            else if (a >= 0 && b < 0 && c >= 0) Console.WriteLine("-");
+            else if (a >= 0 && b >= 0 && c < 0) Console.WriteLine("-");
+        }
+    }
+}
